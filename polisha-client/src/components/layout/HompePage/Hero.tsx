91751814efe74b6fc5b43350099e38829ee5075c
ashimroyrupok <ashimroyrupok@gmail.com>
2025-01-18 22:10:57 +0600

    completed hero section

--- polisha-client/src/components/layout/HompePage/Hero.tsx
+++ polisha-client/src/components/layout/HompePage/Hero.tsx
@@ -1,10 +1,10 @@
-import { ChevronRight } from "lucide-react";
+import { CarTaxiFront, ChevronRight,  FireExtinguisher, PillBottle, RouteOff } from "lucide-react";
 import heroImage from "../../../assets/images/hero.png";
 
 const Hero = () => {
   return (
     <div className=" max-w-7xl mx-auto">
-      <div className="h-screen w-full p-4 md:flex justify-center items-center gap-3 ">
+      <div className=" h-full  py-20 w-full p-4 md:flex justify-center items-center gap-3 ">
         <div className="flex-1 text-center md:text-left">
           <h4
             style={{ letterSpacing: "6px" }}
@@ -40,12 +40,50 @@ const Hero = () => {
           </div>
         </div>
 
-        <div className=" flex-1   h-full w-full flex justify-center items-center">
+        <div className=" flex-1  hidden md:block  h-full w-full">
           <img
             className="  h-[510px]  w-[5400px] aspect-auto "
             src={heroImage}
             alt="polisha hero image"
           />
+        </div>
+      </div>
+
+      {/* hero footer */}
+      <div className=" grid grid-cols-2 lg:grid-cols-4 p-2 gap-8 pb-7">
+        <div className=" relative p-3  border-r-2  ">
+          <CarTaxiFront className="size-20 text-red-500 hover:size-32 " />
+          <h2 className="text-xl font-bold my-4 ">Contactless Washing</h2>
+          <p className="  text-sm  text-opacity-50 ">
+            Vestibulum tortor risus, rutrum at congue sed ultricies finibus
+          </p>
+        </div>
+
+        <div className=" relative  p-3  border-r-2">
+          <PillBottle className="size-20 text-red-500 " />
+          <h2 className="text-xl font-bold my-4 ">Safety Materials</h2>
+          <p className="  text-sm  text-opacity-50 ">
+            Cras aliquam tristique metus, eu gravida diam vestibulum gravida.
+          </p>
+          <div className=" absolute h-10 z-10 bg-red-500 right-0"> </div>
+        </div>
+
+        <div className=" relative p-3 border-r-2 ">
+          <FireExtinguisher className="size-20 text-red-500 " />
+          <h2 className="text-xl font-bold my-4 ">Modern Equipment</h2>
+          <p className="  text-sm  text-opacity-50 ">
+            Fusce maximus molestie nisl, ut dapibus libero vestibulum aliquam.
+          </p>
+          <div className=" absolute h-10 z-10 bg-red-500 right-0"> </div>
+        </div>
+
+        <div className=" relative  p-3 ">
+          <RouteOff className="size-20 text-red-500 " />
+          <h2 className="text-xl font-bold my-4 ">Extensive Cleaning</h2>
+          <p className="  text-sm  text-opacity-50 ">
+            Sestibulum non dolor sit amet mi moles tincidunt vel non velit.
+          </p>
+          <div className=" absolute h-10 z-10 bg-red-500 right-0"> </div>
         </div>
       </div>
     </div>
--- polisha-client/src/redux/api/baseApi.tsx
+++ polisha-client/src/redux/api/baseApi.tsx
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api/v1",
+  baseUrl: "https://polisha-server.vercel.app/api/v1",
   credentials: "include",
 });
 
