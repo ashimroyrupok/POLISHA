a75a84faa267d241ddfa76441a4f2bd0a5d22054
ashimroyrupok <ashimroyrupok@gmail.com>
2025-01-18 17:47:18 +0600

    minor change

--- polisha_server/src/server.ts
+++ polisha_server/src/server.ts
@@ -7,7 +7,7 @@ async function main() {
     await mongoose.connect(config.DATABASE_URL as string);
 
     app.listen(config.PORT, () => {
-      console.log(`Example app listening on port ${config.PORT}`);
+      console.log(` app listening on port ${config.PORT} 🥰`);
     });
   } catch (error) {
     console.log(error);
